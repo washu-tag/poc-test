3401497a5d0c93f9091c76f0928ab912e99ac3d8
Charlie Moore <moore.c@wustl.edu>
2024-10-16 16:56:30 -0500

    Linting attempt

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,8 +1,19 @@
-const eslint = require('@eslint/js');
-const tseslint = require('typescript-eslint');
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+
+const { FlatCompat } = require("@eslint/eslintrc");
+const js = require("@eslint/js");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,                  // optional; default: process.cwd()
+    resolvePluginsRelativeTo: __dirname,       // optional
+    recommendedConfig: js.configs.recommended, // optional unless using "eslint:recommended"
+    allConfig: js.configs.all,                 // optional unless using "eslint:all"
+});
 
 module.exports = tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  ...compat.extends("next/core-web-vitals")
 );
 
